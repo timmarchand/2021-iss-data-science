a8f1daf54852563550f117e3345c04fc65fe3e09
timmarchand <69382807+timmarchand@users.noreply.github.com>
2021-11-11 14:20:56 +0900

    Update variable_associations.R

--- r_docs/variable_associations.R
+++ r_docs/variable_associations.R
@@ -1,20 +1,22 @@
 ## Associations between variables set up ----
 
 ## OPTIONAL clear memory
-# rm(list=ls(all=TRUE))
+ rm(list=ls(all=TRUE))
 
 ## load libraries
 # library(tidyverse)
 # library(ggpubr) # for adding stats to plots
+# library(infer) # for stats tests on tidy data
 # library(vcd) # for chi-squared and mosaic plot
 # library(broom) # for creating tidy tables from regression models
 # library(moderndive) # more easy to read tables from regression models
 # library(effects) # ADVANCED: handling of effects
 # library(sjPlot) # ADVANCED: for plotting of effects
 # library(gapminder) # for data
+# library(janitor) # for tabyl() function
 
 ## Efficeint alternative - use pacman!
-pacman::p_load(tidyverse, ggpubr, vcd, broom, moderndive, effects, sjPlot, gapminder)
+pacman::p_load(tidyverse, ggpubr, infer, vcd, broom, moderndive, effects, sjPlot, gapminder, janitor)
 
 ## load data from data folders ----
 
@@ -30,18 +32,12 @@ resume <- read_csv("https://tinyurl.com/iss-racial-disc-web")
 write_csv(resume,"data/resume.csv")
 
 ## next time, you can load the data directly here
-# resume <- read_csv("data/resume.csv")
+resume <- read_csv("data/resume.csv")
 
 ## get gapminder data
 gapminder <-  gapminder::gapminder
 
-=======
-resume <- read_csv("data/resume.csv")
-resume <- read_csv("https://tinyurl.com/iss-racial-disc-web")
-
-gapminder <-  gapminder::gapminder
-
-## continuous vs categorical:  africa-guess data----
+## continuous vs categorical:  africa-n_africa data----
 ## compare values with box plots or faceted histograms
 ## assess association with regression lines
 ## statistical tests from the t-test family (another form of the linear regression)
@@ -52,7 +48,7 @@ glimpse(df)
 
 ## Try the boxplot again
 ggplot(df) +
-  aes(x = "", y = guess, fill = birthday) +
+  aes(x = "", y = n_africa, fill = birthday) +
   geom_boxplot() +
   scale_fill_hue() +
   labs(x = "Birthday date", y = "Guess for how many countries in Africa", title = "Boxplot of African guesses by odd/even birthday") +
@@ -62,7 +58,7 @@ ggplot(df) +
 
 ## Density plot again
 ggplot(df) +
-  aes(x = guess,fill = birthday) +
+  aes(x = n_africa,fill = birthday) +
   geom_density(adjust = 1,alpha = 0.5) +
   geom_rug(aes(colour=birthday)) +
   labs(x = "Guess for how many countries in Africa", y = "Probability Density", title = "Are guesses related to birthday dates?", fill = "birthday date") +
@@ -73,20 +69,23 @@ ggplot(df) +
 
 ## use the t.test or anova functions
 
+## t.test {Base R} ----
+## requires us to work on vectors
+
 ## but first we need to split the df into two groups using dplyr
 df_odd <- df %>% filter(birthday == "odd")
 df_even <- df %>% filter(birthday == "even")
 
+## Extract vectors from dataframes with $
 ## Try the t.test function
-t.test(df_even$guess,df_odd$guess)
-## as we saw last week, p-value very small!!
-## this gives us the mean values of our two groups (50.4 and 28), a difference of 22
-## the 95% confidence intervals shows this difference could be between 12.4 or 32
-## in 95% of similar samples
+t.test(df_even$n_africa,df_odd$n_africa)
+## as we saw before p-value very small!!
+## this gives us the mean values of our two groups (mean of x vs mean of y)
+## the 95% confidence intervals shows what the difference could be in 95% of similar samples
 
 
 ## You can feed the result into tidy() to get a table of the results
-t.test(df_even$guess,df_odd$guess) %>% broom::tidy()
+t.test(df_even$n_africa,df_odd$n_africa) %>% broom::tidy()
 
 ## estimate = difference in means
 ## estimate 1 = mean of group 1
@@ -94,16 +93,29 @@ t.test(df_even$guess,df_odd$guess) %>% broom::tidy()
 ## statistic = t score
 ## parameter = degrees of freedom
 
+## t_test using {infer} on the dataframe ----
+## The infer package allows us to work on the dataframe directly
+
+df %>% 
+  infer::t_test(formula = n_africa ~ birthday)
+
+## statistic = t score
+## t_df = degrees of freedom
+## alternative = hypothesis chosen (Options are "two-sided" (default), "greater", or "less")
+## lower_ci = lower confidence interval
+## upper_ci = upper confidence interval
+
+##  t-test from regression ----
+
 ## Using regression: any t-test or anova result can be found with a linear regression model
 ## use the function lm(dependent/outcome variable ~ independent/predictor, data = df)
-cat_model <- lm(guess ~ birthday, data = df)
+cat_model <- lm(n_africa ~ birthday, data = df)
 summary(cat_model)
-## (Intercept) estimate = predicted value for the base category (i.e mean for even guess)
-## numberodd estimate = predicted change in value for going from even to odd
+## (Intercept) estimate = predicted value for the base category (i.e mean for even guess of n_africa)
+## birthdayodd estimate = predicted change in value for going from even to odd
 ## Pr(>|t|) = probability, or p-value
 ## Multiple R-squared = correlation coefficient
 ## Adjusted R-squared = adjusted in case of multiple variables
-
 
 ## From moderndive:
 
@@ -114,7 +126,7 @@ get_regression_summaries(cat_model)
 ## nobs = number of observations
 
 get_regression_table(cat_model)
-## (Intercept) estimate = predicted value for the base category (i.e mean for even guess)
+## (Intercept) estimate = predicted value for the base category (i.e mean for even n_africa)
 ## numberodd estimate = predicted change in value for going from even to odd
 ## statistic = t value
 ## lower_ci = lower confidence interval
@@ -122,52 +134,49 @@ get_regression_table(cat_model)
 
 regression_points <- get_regression_points(cat_model)
 regression_points
-## guess_hat shows the predicted values for the model (the means)
-## residuals shows the difference between the fitted (predicted) values and the real (guess) values
+## n_africa_hat shows the predicted values for the model (the means)
+## residuals shows the difference between the fitted (predicted) values and the real (n_africa) values
 
 
 ### ADVANCED STUFF - plotting effects of a linear model with a binary predictor ----
 ## (OKAY TO SKIP, or paste for your own project)
 ## EFFECTS PLOTS show you the nature of the statistical test
 ## Replace the following variables for your own data:
-## guess
+## n_africa
 ## number
 ## df
 ## num (make your own abbreviated name)
 
 
-## using sjPlot
+## using {sjPlot}
 ## assign the model to model.01
 
 model.01 <- cat_model
 set_theme(base = theme_sjplot())
-plot_model(model.01, type = "pred", terms = c("count_task"))
+plot_model(model.01, type = "pred", , terms = c("birthday"))
 
-
-# install.packages(effects)  # once per machine
-library(effects)
-
-## A linear model with a binary predictor
-## -> t-test for independent samples
-library(effects)
-summary(model.01 <- lm(guess ~ birthday, data = df))
+## using {effects} package
 
 ### determine the nature of the effect(s) numerically
-cbind(coef(model.01), confint(model.01))
+
+# returns the estimates and 95% confidence interval values
+cbind(coef(model.01), confint(model.01)) 
 (preds.hyp <- data.frame(num <- effect("birthday", model.01)))
 
-head(predict(model.01)) # the 'predictions' of the model for the current data
+## check which parts match the get_regression_table output!
+
+# get the 'predictions' of the model for the current data using predict
+head(predict(model.01)) 
+## check which parts match the get_regression_points output!
 
 ### determine the nature of the effect(s) graphically
-plot(num, ylim=range(df$guess), xlab="Birthday date even_odd", grid=TRUE)     # prediction
+plot(num, ylim=range(df$n_africa), xlab="Birthday date even_odd", grid=TRUE)     # prediction
 
 
 ## continuous vs categorical:  real world data----
 ## compare values with box plots or faceted histograms
 ## assess association with regression lines
 ## statistical tests from the t-test family (another form of the linear regression)
-
-
 
 ## Examine whether there is a difference associated between continent and life expectancy in 2007
 
@@ -222,10 +231,27 @@ get_regression_points(lifeExp_model, ID = "country")
 ## EFFECTS PLOTS show you the nature of the statistical test
 ## variable names have been replaced
 
-model.01 <- lifeExp_model
 set_theme(base = theme_light())
+plot_model(lifeExp_model, type = "pred", terms = c("continent")) +
+  theme_sjplot()
 
-plot_model(model.01, type = "pred", terms = c("continent")) +theme_sjplot()
+
+## Base R and {effects} alternative
+## Alternative to get_regression_table(lifeExp_model)
+(preds.hyp <- data.frame(num <- effect("continent", lifeExp_model)))
+
+# get the 'predictions' of the model for the current data using predict
+## Alternative to get_regression_points(lifeExp_model)
+gapminder2007 %>% 
+  mutate(lifeExp_hat = predict(lifeExp_model),
+         residual = lifeExp - lifeExp_hat)
+ 
+### determine the nature of the effect(s) graphically
+## Alternative to plot_model
+## Get effect details
+num <- effect("continent", lifeExp_model)
+## Plot in Base R
+plot(num,  xlab="Continent", grid=TRUE)     # prediction
 
 
 ## categorical vs categorical ----
@@ -233,12 +259,11 @@ plot_model(model.01, type = "pred", terms = c("continent")) +theme_sjplot()
 ## statistical test from the Chi-squared family
 ## plot joint frequencies with bar plots or mosaic plots
 
+## Use {Base R} table()
 attach(df)
 table(birthday)
 table(visit)
-<<<<<<< HEAD
 table(birthday,visit)
-=======
 
 # get a table of the proportions
 prop.table(table(visit)) # values between 0 ~ 1
@@ -249,7 +274,6 @@ round(prop.table(table(birthday))*100)
 
 ## to get a relative frequency table, use the margin argument
 # margin = 2 selects the columns, so now the columns add up to 100%
-
 round(prop.table(table(birthday,visit),margin = 2)*100)
 # margin = 1 selects the rows, so now the rows add up to 100%
 round(prop.table(table(birthday,visit),margin = 1)*100)
@@ -259,27 +283,42 @@ table(birthday, visit) # first value for rows, second value for columns
 
 ## easily get the chi-squared value using summary()
 summary(table(birthday, visit))
-# p-value = 0.9791 - of course, no significant difference
+# p-value > 0.9 - of course, no significant difference
 # Chi-squared approximation may be incorrect message - because 'yes' column very small
 
 # use fisher exact test as alternative
 fisher.test(birthday,visit) # p-value = 1!!
+detach(df)
 
+## Alternative to tables with {dplyr} and {janitor} ----
+## Use count to create a tidy dataframe
+df %>% count(birthday)
+df %>% count(visit)
+df %>% count(birthday,visit)
+## Add proportions
+df %>% count(birthday, visit) %>%  
+  mutate(prop = prop.table(n))
+
+## Alternative with {janitor}
+df %>% tabyl(birthday,visit) %>% adorn_percentages()
+
+## Chisq_test with {infer} ----
+df %>% chisq_test(formula = birthday ~ visit)
 
 
 ## plotting bar chart with ggplot
 ggplot(df) +
   aes(x = visit, fill = birthday) +
   geom_bar() +
-  scale_fill_viridis_d(option = "cividis") +
-  coord_flip() +
+ # scale_fill_viridis_d(option = "cividis") + # for a fancy colour scheme
+  coord_flip() + # change x and y axis
   theme_minimal()
 
 ## plotting with a mosaic plot from vcd package
-## some useful stats
-<<<<<<< HEAD
+## some useful stats from assocstats() function
+attach(df)
 assocstats(table(birthday, visit))
-# Pearson value same as the p=value above
+# Pearson value same as the p_value above
 
 ##the mosaic plot
 mosaic(~ birthday + visit,
@@ -308,37 +347,51 @@ detach(df)
 
 glimpse(resume)
 
+
+### Base R and table()
+
 ## as before, you can get a contingency table with table and two column vectors
 (table(resume$race,resume$call))
 ## or a table of percentages, rounded down
 round(prop.table(table(resume$race,resume$call))*100,1) # rounded to one decimal place
 
+
 ## easily get the chi-squared value using summary()
 summary((table(resume$race,resume$call)))# p-value < 0.001
 
+
+### Tidy alternative with {infer}
+
+resume %>% mutate(call = factor(call)) %>% # change call to a factor (categorical) variable
+ # mutate(call = factor(call)) %>%   ##  the response variable is expected to be categorical error
+  infer::chisq_test(call ~ race)
+
+
 ## plotting bar chart with ggplot
-ggplot(resume) +
-  aes(x = race, fill = call) +
+resume %>%  
+  ggplot(aes(x = race, fill = call)) +
   geom_bar() +
   scale_fill_viridis_d(option = "cividis") +
   coord_flip() +
   theme_minimal()
+
+## Not the best plot ever!
 
 ## check the problem with the variables
 class(resume$race)
 class(resume$call)
 ## call is saved as a numeric variable!
 
-## change to logical
-resume$call <- as.logical(resume$call)
-
-## try again
-ggplot(resume) +
-  aes(x = race, fill = call) +
+## change to logical and try again
+## plotting bar chart with ggplot
+resume %>% mutate(call = factor(call)) %>% 
+  ggplot(aes(x = race, fill = call)) +
   geom_bar() +
   scale_fill_viridis_d(option = "cividis") +
   coord_flip() +
   theme_minimal()
+
+
 
 ## plotting with a mosaic plot from vcd package
 
@@ -374,28 +427,29 @@ mosaic(~ race + call,
 ## assess association with regression lines
 ## statistical tests from the R-squared family (or the linear model of regression)
 
-## Let's revisit the df example of height X guess
+## Let's revisit the df example of height X n_africa
 ## we saw the following code when we looked at in week_5_hw_plots:
 
 ggplot(df) +
-  aes(x = height, y = guess) +
+  aes(x = height, y = n_africa) +
   geom_point() +
   geom_smooth(method = "lm")  +
   ggpubr::stat_cor(method="pearson") +
   theme_minimal()
 
 ## simple correlations without plotting
+## working on vectors
 attach(df)
 ## this give the the R value, or slope of the line
-cor(x=height, y=guess)
+cor(x=height, y=n_africa)
 ## you can choose a method depending on the nature of the data
-cor(x = height, y = guess, method = "spearman")
+cor(x = height, y = n_africa, method = "spearman")
 
 ## to get other statistics, you can use cor.test
-cor.test(height,guess, method = "pearson")
+cor.test(height,n_africa, method = "pearson")
 
 ## if you save the result as an object...
-res <- cor.test(height,guess, method = "pearson")
+res <- cor.test(height,n_africa, method = "pearson")
 
 ## you can produce a nice table using the broom::tidy function
 broom::tidy(res)
@@ -405,19 +459,22 @@ broom::tidy(res)
 ## conf.low = lower 95% confidence level
 ## conf.high = upper 95% confidence level
 
-## in this case the confidence levels go from -0.133 to 0.417
+## in this case the confidence levels go from -0.XXX to 0.XXX
 ## if it crosses over 0, there's a good chance there is no relationship at all
 ## as evidenced by the high p.value
 
 ## note that for many correlation tests, the variables are assumed to be distributed normally
 ## always good practice to check that first
+detach(df)
+
+
 
 ## QUESTION - how can you check variables have normal distribution? ----
 
 
 ## linear regression models for two continuous variables ----
 
-cont_model <- lm(formula = guess ~ height, data = df)
+cont_model <- lm(formula = n_africa ~ height, data = df)
 summary(cont_model)
 get_regression_summaries(cont_model)
 get_regression_table(cont_model)
@@ -426,7 +483,7 @@ get_regression_points(cont_model)
 
 ## Our original plot, but now with expanded x and y axes
 ggplot(df) +
-  aes(x = height, y = guess) +
+  aes(x = height, y = n_africa) +
   geom_point() +
   geom_smooth(method = "lm")  +
   xlim(0,200) +
@@ -439,7 +496,7 @@ ggplot(df) +
 ## and the the slope value as the estimate for height
 
 ggplot(df) +
-  aes(x = height, y = guess) +
+  aes(x = height, y = n_africa) +
   geom_point() +
   geom_smooth(method = "lm")  +
  geom_abline(mapping=aes(slope=0.359, intercept=-19.5)) +

c37a58f4aebba0dd2e68a09e2ed390fda43b68ef
timmarchand <69382807+timmarchand@users.noreply.github.com>
2025-05-13 10:51:58 +0900

    Update update_folder.R
    Added dplyr:: etc to avoid conflicts

--- r_docs/update_folder.R
+++ r_docs/update_folder.R
@@ -2,19 +2,23 @@
 
 library(tidyverse)
 
-update_folder <- function(week = 1:14,type = c("r_docs","PS","data","figs","R","Rmd"),...){
-  read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRimSFdlfx8dNYlqvLHEwdipABwqWSaM7Si2iYgV7wKEDnIo2-f3LJiqBWTzaZ-MuU8cY-3zus6-dEH/pub?gid=1828289266&single=true&output=csv", col_types = cols()) %>% 
-    filter(week %in% {{week}}, type %in% {{type}}) %>% 
-    mutate(path = case_when(type == "figs" ~ paste0("figures/",name),
-                            type == "data" ~ paste0("data/",name),
-                            type == "R" ~ paste0("r_docs/", name),
-                            type == "Rmd" ~ paste0("r_docs/", name),
-                            type == "r_docs" ~ paste0("r_docs/", name),
-                            type == "PS" ~ paste0("prob_sets/", name),
-                            type == "base" ~ name,
-                            TRUE ~ "ignore")) %>%
-    filter(path != "ignore") %>% 
-    filter(include) %>% 
-    select(url,path) %>% as.list %>% 
-    pwalk(.f = function(url,path){download.file(url,path)})
+update_folder <- function(week = 1:14, type = c("r_docs", "PS", "data", "figs", "R", "Rmd"), ...) {
+  readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRimSFdlfx8dNYlqvLHEwdipABwqWSaM7Si2iYgV7wKEDnIo2-f3LJiqBWTzaZ-MuU8cY-3zus6-dEH/pub?gid=1828289266&single=true&output=csv", col_types = cols()) %>%
+     dplyr::filter(week %in% {{week}}, type %in% {{type}}) %>%
+    dplyr::mutate(path = case_when(
+      type == "figs" ~ paste0("figures/", name),
+      type == "data" ~ paste0("data/", name),
+      type == "R" ~ paste0("r_docs/", name),
+      type == "Rmd" ~ paste0("r_docs/", name),
+      type == "r_docs" ~ paste0("r_docs/", name),
+      type == "PS" ~ paste0("prob_sets/", name),
+      type == "base" ~ name,
+      TRUE ~ "ignore"
+    )) %>%
+    dplyr::filter(include) %>% 
+    dplyr::select(url, path) %>%
+    as.list() %>%
+    purrr::pwalk(.f = function(url, path) {
+      download.file(url, path)
+    })
 }

26085ac803a7c102fde7dca220a472e570285fe9
timmarchand <69382807+timmarchand@users.noreply.github.com>
2025-11-11 13:05:51 +0900

    Update update_project.R

--- update_project.R
+++ update_project.R
@@ -1,19 +1,4 @@
-# setup ----
-library(tidyverse)
-source("r_docs/update_folder.R")
+## Code to load updated class materials
 
-# Instructions ----
-## select the week you want to download the data for, and the type of data:
-## "figs" for figures and images
-## "r_docs" for R scripts and R Markdown
-## "data" for csv and other data files
-## "PS" for problem sets
-
-## leave blank to download all available (this will take longer)
-
-
-## e.g. update_folder(week = 2) --> for all types from week 2; 
-## e.g. update_folder(week = 1:3, "r_docs") --> for first 3 weeks r_docs only
-## e.g. update_folder() --> all available types for all available weeks
-
+source("https://tinyurl.com/folderUpdate")
 update_folder()
